c8e83e5dfa81333a78a23cac02bc9c50667fd30c
Anto Akoke Victor <antoakoke@gmail.com>
2023-05-29 23:48:12 +0100

    create cart and cartitem migrations

---
+++ database/migrations/2023_05_29_223203_create_cart_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\{Cart, Product};
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->foreignIdFor(Cart::class);
+            $table->foreignIdFor(Product::class);
+            $table->unsignedInteger('qty');
+            $table->primary(array('cart_id', 'product_id'));
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
