4a4740c6c839f388ce39f99642ccadc54aefd174
Anto Akoke Victor <antoakoke@gmail.com>
2023-05-30 17:11:03 +0100

    add migration model and controller for orders

---
+++ database/migrations/2023_05_30_153509_create_orders_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * 
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->string('cart_key');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('company');
+            $table->string('address');
+            $table->string('country');
+            $table->string('state');
+            $table->string('city');
+            $table->string('zip');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('shipping_method');
+            $table->string('transaction_id');
+            $table->string('status');
+            $table->decimal('total_price');
+            $table->string('reference');
+            $table->foreign('cart_key')->references('key')->on('carts');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
