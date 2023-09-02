896bf5d3fb71a5a80105ddf6ddf1e1b14cfc4bba
Anto Akoke Victor <antoakoke@gmail.com>
2023-09-02 22:06:27 +0100

    view resources on guest

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -17,9 +17,10 @@ class ProductResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'image' => $this->image,
             'description' => $this->description,
             'category_type' => $this->category_type,
-            'colors' => $this->colors,
+            'colors' => ColorResource::collection($this->colors),
             'status' => $this->status,
             'slug' => $this->slug,
             'created_at' => $this->created,
