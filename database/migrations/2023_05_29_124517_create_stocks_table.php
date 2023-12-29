2aea9f9b44930951026fae61098ad34e5c454f10
Anto Akoke Victor <antoakoke@gmail.com>
2023-12-29 17:50:58 +0100

    generate new migrations for new structure

--- database/migrations/2023_05_29_124517_create_stocks_table.php
+++ database/migrations/2023_05_29_124517_create_stocks_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Size;
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('stocks', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Size::class);
+            $table->foreignIdFor(Product::class);
             $table->string('sku')->nullable(false);
             $table->integer('init_qty')->nullable(false);
             $table->integer('available_qty')->nullable(false);
